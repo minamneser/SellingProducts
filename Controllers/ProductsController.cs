54dea9198b37f28aad3a3ad0e70d47b1efbcee03
minamneser <minamneser@gmail.com>
2024-08-27 21:51:10 +0300

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -31,7 +31,6 @@ namespace SellingProducts.Controllers
         public IActionResult Create()
         {
             createSelectList();
-            //Test
             return View();
         }
         [Authorize]
